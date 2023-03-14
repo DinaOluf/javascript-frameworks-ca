4a457caa40aa8a301735139614c9adcece8fc1ac
Dina <dina.olufsen@hotmail.com>
2023-03-14 18:12:04 +0100

    preparing for content
    - Downloaded styled components, react router and react hook form.
    - Added files, icons and fonts
    - Barely started building out structure of website.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles.scss';
+import React, { useState, useEffect, useReducer, createContext, useContext } from 'react'; //Check for unused
+import { Routes, Route, Link, Outlet, useParams } from 'react-router-dom'; //Check for unused
+import useApi from './components/useApi';
+
+
+function Home() {
+  const { data, isLoading, isError } = useApi(
+    'https://api.noroff.dev/api/v1/online-shop',
+  );
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  console.log(data); //Remove
+
+  return <div>
+      <div>
+        Home
+      </div>
+      <div>
+        Data loaded
+      </div>
+    </div>;
+}
+
+function Contact() {
+  return <div>Contact</div>;
+}
+
+function Cart() {
+  return <div>Cart</div>;
+}
+
+
+function Product() {
+  let params = useParams();
+  // console.log(params);
+  // Logs the id of whichever product page you are on e.g.
+  // {id: '1'} or {id: '2'}
+  return <div>Individual product page: {params.id}</div>;
+}
+
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div>Header with Logo and nav</div>
+      <Nav />
+    </header>
+  );
+}
+
+function Footer() {
+  return <footer>Website footer</footer>;
+}
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+        <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="product/:id" element={<Product />} />
+          <Route path="*" element={<RouteNotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
